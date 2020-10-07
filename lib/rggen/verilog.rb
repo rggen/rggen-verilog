4debc45fc9c2a6c3c066fc0711d97ed8db5c2c61
Taichi Ishitani <taichi730@gmail.com>
2020-10-07 23:21:18 +0900

    added w0src, w1src and wsrc bit field types

--- lib/rggen/verilog.rb
+++ lib/rggen/verilog.rb
@@ -23,6 +23,7 @@ module RgGen
       'verilog/bit_field/type/rwl',
       'verilog/bit_field/type/rws',
       'verilog/bit_field/type/w0crs_w1crs_wcrs',
+      'verilog/bit_field/type/w0src_w1src_wsrc',
       'verilog/bit_field/verilog_top',
       'verilog/register/type',
       'verilog/register/type/external',
