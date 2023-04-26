8a337f4a1fe5ea123e581629ecc037b36a8f536e
Taichi Ishitani <taichi730@gmail.com>
2023-04-26 22:37:12 +0900

    support bit field label (#31, rggen/rggen#126)

--- spec/rggen/verilog/rtl_header/bit_field/verilog_rtl_header_spec.rb
+++ spec/rggen/verilog/rtl_header/bit_field/verilog_rtl_header_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'bit_field/verilog_rtl_header' do
     RgGen.enable(:register_file, [:name, :offset_address, :size])
     RgGen.enable(:register, [:name, :offset_address, :size, :type])
     RgGen.enable(:register, :type, [:external, :indirect])
-    RgGen.enable(:bit_field, [:name, :bit_assignment, :type, :initial_value, :reference])
+    RgGen.enable(:bit_field, [:name, :bit_assignment, :type, :initial_value, :reference, :labels])
     RgGen.enable(:bit_field, :type, [:rw])
     RgGen.enable(:bit_field, :verilog_rtl_header)
   end
@@ -30,7 +30,17 @@ RSpec.describe 'bit_field/verilog_rtl_header' do
 
         register do
           name 'register_1'
-          bit_field { bit_assignment lsb: 0, width: 64; type :rw; initial_value 0 }
+          bit_field {
+            bit_assignment lsb: 0, width: 64
+            type :rw
+            initial_value 0
+            labels [
+              { name: 'FOO', value: 0 },
+              { name: 'BAR', value: 2**16 - 1 },
+              { name: 'BAZ', value: 2**32 - 1 },
+              { name: 'QUX', value: 2**64 - 1 }
+            ]
+          }
         end
 
         register do
@@ -66,7 +76,16 @@ RSpec.describe 'bit_field/verilog_rtl_header' do
             register do
               name 'register_4_2_1'
               size [2, 2]
-              bit_field { name 'bit_field_0'; bit_assignment lsb:  0, width:  1; type :rw; initial_value 0 }
+              bit_field {
+                name 'bit_field_0'
+                bit_assignment lsb:  0, width:  1
+                type :rw
+                initial_value 0
+                labels [
+                  { name: 'FIZZ', value: 0 },
+                  { name: 'BUZZ', value: 1 }
+                ]
+              }
             end
           end
         end
@@ -96,7 +115,11 @@ RSpec.describe 'bit_field/verilog_rtl_header' do
       expect(verilog_rtl_header[3].macro_definitions).to match([
         match_macro_definition('BLOCK_0_REGISTER_1_BIT_WIDTH', 64),
         match_macro_definition('BLOCK_0_REGISTER_1_BIT_MASK', "64'hffffffffffffffff"),
-        match_macro_definition('BLOCK_0_REGISTER_1_BIT_OFFSET', 0)
+        match_macro_definition('BLOCK_0_REGISTER_1_BIT_OFFSET', 0),
+        match_macro_definition('BLOCK_0_REGISTER_1_FOO', "64'h0000000000000000"),
+        match_macro_definition('BLOCK_0_REGISTER_1_BAR', "64'h000000000000ffff"),
+        match_macro_definition('BLOCK_0_REGISTER_1_BAZ', "64'h00000000ffffffff"),
+        match_macro_definition('BLOCK_0_REGISTER_1_QUX', "64'hffffffffffffffff")
       ])
 
       expect(verilog_rtl_header[4].macro_definitions).to match([
@@ -144,7 +167,9 @@ RSpec.describe 'bit_field/verilog_rtl_header' do
       expect(verilog_rtl_header[10].macro_definitions).to match([
         match_macro_definition('BLOCK_0_REGISTER_FILE_4_REGISTER_FILE_4_2_REGISTER_4_2_1_BIT_FIELD_0_BIT_WIDTH', 1),
         match_macro_definition('BLOCK_0_REGISTER_FILE_4_REGISTER_FILE_4_2_REGISTER_4_2_1_BIT_FIELD_0_BIT_MASK', "1'h1"),
-        match_macro_definition('BLOCK_0_REGISTER_FILE_4_REGISTER_FILE_4_2_REGISTER_4_2_1_BIT_FIELD_0_BIT_OFFSET', 0)
+        match_macro_definition('BLOCK_0_REGISTER_FILE_4_REGISTER_FILE_4_2_REGISTER_4_2_1_BIT_FIELD_0_BIT_OFFSET', 0),
+        match_macro_definition('BLOCK_0_REGISTER_FILE_4_REGISTER_FILE_4_2_REGISTER_4_2_1_BIT_FIELD_0_FIZZ', "1'h0"),
+        match_macro_definition('BLOCK_0_REGISTER_FILE_4_REGISTER_FILE_4_2_REGISTER_4_2_1_BIT_FIELD_0_BUZZ', "1'h1")
       ])
     end
   end
