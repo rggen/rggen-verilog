0f9937d4cd93554d0e9d07f54b316fa4d77757a0
Taichi Ishitani <taichi730@gmail.com>
2024-11-22 14:12:16 +0900

    change indirect index logic
    (refs: rggen/rggen#224)

--- lib/rggen/verilog/rtl/register/type/indirect.rb
+++ lib/rggen/verilog/rtl/register/type/indirect.rb
@@ -5,14 +5,20 @@ RgGen.define_list_item_feature(:register, :type, :indirect) do
     include RgGen::SystemVerilog::RTL::IndirectIndex
 
     build do
-      wire :indirect_index, {
-        name: 'w_indirect_index', width: index_width
+      wire :indirect_match, {
+        name: 'w_indirect_match', width: index_match_width
       }
     end
 
     main_code :register do |code|
-      code << indirect_index_assignment << nl
+      indirect_index_matches(code)
       code << process_template
+    end
+
+    private
+
+    def array_index_value(value, width)
+      value[0, width]
     end
   end
 end
